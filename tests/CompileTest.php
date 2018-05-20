46c0825e198dbf125fba151521c3e014c24951a3
Christopher Pitt <cgpitt@gmail.com>
2018-05-20 20:28:41 +0200

    Add translate to the cover, since that's what compile is using

--- tests/CompileTest.php
+++ tests/CompileTest.php
@@ -2,11 +2,12 @@
 
 /**
  * @covers Pre\Phpx\Parser::compile
+ * @covers Pre\Phpx\Parser::translate
  */
 class CompileTest extends TestCase
 {
     public function test_can_compile()
-    {   
+    {
         $this->assertEquals(
             $this->fixtureCode("compile/can-compile", "php"),
             \Pre\Phpx\Parser::compile($this->fixtureCode("compile/can-compile", "pre"))
